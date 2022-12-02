052efeaddea9d5d5f2adfd2e67b138efd696e0f7
Paul Statezny <paulstatezny@gmail.com>
2022-12-02 11:20:44 -0700

    Add part2 solution for day 2

--- 2.go
+++ 2.go
@@ -6,48 +6,35 @@ import (
 	"os"
 )
 
+/*
+ * SEMANTIC TYPES
+ *
+ * To help make the code easier to reason about.
+ */
+
 type Choice int
 type Outcome int
 
+// A type to represent a Choice that a player makes in a Rock, Paper, Scissors game
 const (
 	Rock Choice = iota
 	Paper
 	Scissors
 )
 
+// A type to represent the Outcome of a Rock, Paper, Scissors game
 const (
 	Win Outcome = iota
 	Tie
 	Loss
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
+/*
+ * READING AND PARSING THE TEXT FILE
+ */
 
-func main() {
-	readFile, err := os.Open("2.txt")
-
-	check(err)
-
-	fileScanner := bufio.NewScanner(readFile)
-	fileScanner.Split(bufio.ScanLines)
-
-	score := 0
-
-	for fileScanner.Scan() {
-		text := []rune(fileScanner.Text())
-		self := choice(text[2])
-		opponent := choice(text[0])
-		score += roundScore(self, opponent)
-	}
-
-	fmt.Println("Total score:", score)
-}
-
-func choice(r rune) Choice {
+// Deserialize a "Choice" rune into a Choice type
+func parseChoice(r rune) Choice {
 	switch r {
 	case 'A', 'X':
 		return Rock
@@ -60,40 +47,146 @@ func choice(r rune) Choice {
 	}
 }
 
-func roundScore(self Choice, opponent Choice) int {
-	score := 0
-
-	switch self {
-	case Rock:
-		score += 1
-
-	case Paper:
-		score += 2
-
-	case Scissors:
-		score += 3
+// Deserialize an "Outcome" rune into an Outcome type
+func parseOutcome(r rune) Outcome {
+	switch r {
+	case 'X':
+		return Loss
+	case 'Y':
+		return Tie
+	case 'Z':
+		return Win
+	default:
+		panic("invalid input")
 	}
-
-	switch outcome(self, opponent) {
-	case Win:
-		score += 6
-	case Tie:
-		score += 3
-	case Loss:
-		score += 0
-	}
-
-	return score
 }
 
-func outcome(a Choice, b Choice) Outcome {
-	if a == b {
+// Return a bufio.Scanner to iterate through lines of a text file
+func lineScanner() (*os.File, *bufio.Scanner) {
+	file, err := os.Open("2.txt")
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	return file, scanner
+}
+
+func closeFile(f *os.File) {
+	err := f.Close()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+// Get a single line of text as a []rune from a bufio.Scanner
+func getLine(scanner *bufio.Scanner) []rune {
+	return []rune(scanner.Text())
+}
+
+/*
+ * LOGIC UTILITIES
+ *
+ * Simple functions that return atomic facts or decisions about a scenario in a
+ * Rock, Paper, Scissors game.
+ */
+
+// Given the choices of the self and opponent, return the Outcome
+// from the perspective of the self. ("Did I win, lose, or tie?")
+func outcomeOfRound(self Choice, opponent Choice) Outcome {
+	if self == opponent {
 		return Tie
 	}
 
-	if (a-b+3)%3 == 1 {
+	if (self-opponent+3)%3 == 1 {
 		return Win
 	} else {
 		return Loss
 	}
 }
+
+// Given a Choice, return how many points should be tallied
+func choiceScore(choice Choice) int {
+	switch choice {
+	case Rock:
+		return 1
+
+	case Paper:
+		return 2
+
+	default:
+		return 3
+	}
+}
+
+// Given an Outcome, return how many points should be tallied
+func outcomeScore(outcome Outcome) int {
+	switch outcome {
+	case Win:
+		return 6
+	case Tie:
+		return 3
+	default:
+		return 0
+	}
+}
+
+// Given an opponent Choice and a desired Outcome, return
+// the Choice that the self should make in order to achieve
+// the Outcome
+func selfChoice(opponent Choice, outcome Outcome) Choice {
+	switch outcome {
+	case Win:
+		return (opponent + 1) % 3
+	case Tie:
+		return opponent
+	default:
+		return (opponent - 1 + 3) % 3
+	}
+}
+
+/*
+ * PROGRAM ENTRY POINTS
+ *
+ * For parts 1+2 of day 2 of Advent of Code 2022.
+ */
+
+// Part 1
+func Part1Main() {
+	file, scanner := lineScanner()
+	defer closeFile(file)
+	score := 0
+
+	for scanner.Scan() {
+		line := getLine(scanner)
+		self := choice(line[2])
+		opponent := choice(line[0])
+		score += choiceScore(self) + outcomeScore(outcomeOfRound(self, opponent))
+	}
+
+	fmt.Println("Total score:", score)
+}
+
+// Part 2
+func main() {
+	file, scanner := lineScanner()
+	defer closeFile(file)
+	score := 0
+
+	for scanner.Scan() {
+		line := getLine(scanner)
+		opponent := parseChoice(line[0])
+		outcome := parseOutcome(line[2])
+		self := selfChoice(opponent, outcome)
+		score += choiceScore(self) + outcomeScore(outcome)
+	}
+
+	fmt.Println("Total score:", score)
+}
+
