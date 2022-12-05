5f5014f0e9b73491ea27656a1a2ce9251eb59b8b
Paul Statezny <paulstatezny@gmail.com>
2022-12-05 11:05:24 -0700

    Finish part2 of day 3

--- 3.go
+++ 3.go
@@ -4,14 +4,19 @@ import (
 	"fmt"
 )
 
-type RuckSack struct {
+type RuckSack1 struct {
 	// compartment1 is stored as a map (effectively a set) to keep searching O(n)
 	// instead of O(n^2)
 	compartment1 map[rune]bool
 	compartment2 []rune
 }
 
-func main() {
+type RuckSack2 struct {
+	// stored as a map (effectively a set) to keep searching O(n) instead of O(n^2)
+	items map[rune]bool
+}
+
+func part1Main() {
 	file, scanner := LineScanner("3.txt")
 	defer CloseFile(file)
 
@@ -19,8 +24,8 @@ func main() {
 
 	for scanner.Scan() {
 		line := GetLine(scanner)
-		ruckSack := ruckSack(line)
-		commonItem := commonItem(ruckSack)
+		ruckSack := ruckSack1(line)
+		commonItem := commonItem1(ruckSack)
 		priority := priority(commonItem)
 		totalPriority += priority
 	}
@@ -28,8 +33,30 @@ func main() {
 	fmt.Println("Sum of priorities:", totalPriority)
 }
 
-// Get a RuckSack from a line of text
-func ruckSack(line []rune) RuckSack {
+func main() {
+	file, scanner := LineScanner("3.txt")
+	defer CloseFile(file)
+
+	totalPriority := 0
+	group := []RuckSack2{}
+
+	for scanner.Scan() {
+		line := GetLine(scanner)
+		ruckSack := ruckSack2(line)
+		group = append(group, ruckSack)
+
+		if len(group) == 3 {
+			commonItem := commonItem2(group)
+			totalPriority += priority(commonItem)
+			group = make([]RuckSack2, 0)
+		}
+	}
+
+	fmt.Println("Sum of priorities:", totalPriority)
+}
+
+// Get a RuckSack1 from a line of text
+func ruckSack1(line []rune) RuckSack1 {
 	compartmentSize := len(line) / 2
 	var compartment1 []rune = line[:compartmentSize]
 	compartment2 := line[compartmentSize:]
@@ -39,15 +66,49 @@ func ruckSack(line []rune) RuckSack {
 		compartment1Map[item] = true
 	}
 
-	return RuckSack{compartment1Map, compartment2}
+	return RuckSack1{compartment1Map, compartment2}
+}
+
+// Get a RuckSack2 from a line of text
+func ruckSack2(line []rune) RuckSack2 {
+	items := make(map[rune]bool)
+
+	for _, item := range line {
+		items[item] = true
+	}
+
+	return RuckSack2{items}
 }
 
 // Find the common item between the two compartments of a ruck sack
-func commonItem(sack RuckSack) rune {
+func commonItem1(sack RuckSack1) rune {
 	for _, item := range sack.compartment2 {
 		if sack.compartment1[item] == true {
 			return item
 		}
+	}
+
+	return '0'
+}
+
+// Find the common item between 3 separate RuckSack2s
+func commonItem2(sacks []RuckSack2) rune {
+	matches := sacks[0].items
+
+	for _, sack := range sacks[1:] {
+		for index, _ := range matches {
+			if sack.items[index] == false {
+				delete(matches, index)
+			}
+		}
+	}
+
+	if len(matches) != 1 {
+		panic("More than 1 common item between 3 RuckSacks")
+	}
+
+	for item, _ := range matches {
+		return item
 	}
 
 	return '0'
